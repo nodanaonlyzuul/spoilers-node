2422b33da8bd3623a78062e197334b305328426a
Stephen Schor <sschor@movableink.com>
2014-04-23 18:40:38 -0400

    implement and remove boilerplate

--- app.js
+++ app.js
@@ -1,30 +1,20 @@
 // dependencies
 var express       = require('express');
 var path          = require('path');
-var favicon       = require('static-favicon');
 var logger        = require('morgan');
 var cookieParser  = require('cookie-parser');
 var bodyParser    = require('body-parser');
 
-var routes    = require('./routes/index');
-var users     = require('./routes/users');
+// Routes
 var spoilers  = require('./routes/spoilers');
 
 var app = express();
 
-// view engine setup
-app.set('views', path.join(__dirname, 'views'));
-app.set('view engine', 'jade');
-
-app.use(favicon());
 app.use(logger('dev'));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded());
 app.use(cookieParser());
-app.use(express.static(path.join(__dirname, 'public')));
 
-app.use('/', routes);
-app.use('/users', users);
 app.use('/', spoilers);
 
 /// catch 404 and forwarding to error handler
@@ -57,6 +47,5 @@ app.use(function(err, req, res, next) {
         error: {}
     });
 });
-
 
 module.exports = app;
--- routes/spoilers.js
+++ routes/spoilers.js
@@ -1,9 +1,36 @@
+// Lib
 var express = require('express');
-var router = express.Router();
+var router  = express.Router();
+yaml        = require('js-yaml')
+var fs      = require('fs');
 
-/* GET home page. */
+// Locals
+var spoilers = yaml.safeLoad(fs.readFileSync('./resources/spoilers.yml', 'utf8'));
+
+
+router.get("/", function(req, res){
+  res.redirect("/spoiler-topics");
+})
+
 router.get('/spoiler-topics', function(req, res) {
-  res.send("spoiler-list-here");
+  topics = []
+  for (i in spoilers) { topics.push(i); }
+
+  res.send(topics.sort());
+});
+
+
+router.get('/spoilers', function(req, res) {
+  res.send(spoilers);
+});
+
+
+router.get('/spoilers/:topic', function(req, res){
+  topic   = req.params.topic;
+  spoiler = spoilers[topic];
+  message = (spoiler) ? spoiler : "could not be be found";
+
+  res.send({spoiler: message});
 });
 
 module.exports = router;
